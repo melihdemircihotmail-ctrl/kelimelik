b70b7d963dbf8ad96730b11278f9a126c3c2053e
melihdemircihotmail-ctrl <melihdemircihotmail@gmail.com>
2026-09-11 15:30:11 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'kelimelik-v6';
+var CACHE = 'kelimelik-v7';
 var FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-180.png'];
 
 self.addEventListener('install', function(e){
